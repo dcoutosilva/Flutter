6199af0cfadd759fb4fcaf2ac42638f09cd11823
Danilo Couto <d.couto.silva@gmail.com>
2021-04-29 18:03:54 -0300

    aula 39

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import './questao.dart';
-import './resposta.dart';
+import './questionario.dart';
 import './resultado.dart';
 
 main() {
@@ -28,10 +27,6 @@ class _PerguntasAppState extends State<PerguntasApp> {
     }
   }
 
-  bool get temPerguntaSelecionada {
-    return _perguntaSelecionada < _perguntas.length;
-  }
-
   /* void Function() funcaoQueRetornaOutraFuncao() {
     return () {
       print('Pergunta Respondida #02');
@@ -40,10 +35,6 @@ class _PerguntasAppState extends State<PerguntasApp> {
 
   @override
   Widget build(BuildContext context) {
-    List<String> respostas = temPerguntaSelecionada
-        ? _perguntas[_perguntaSelecionada]['respostas']
-        : null;
-
     //tem pergunta selecionada, se sim, ele entra, se não vai pra null
 
     // for (String in textoResp in respostas ){
@@ -57,29 +48,10 @@ class _PerguntasAppState extends State<PerguntasApp> {
           title: Text('perguntas'),
         ),
         body: temPerguntaSelecionada
-            ? Column(
-                children: [
-                  Questao(_perguntas[_perguntaSelecionada]['texto']),
-                  ...respostas.map((t) => Resposta(t, _responder)).toList(),
-                  /* Resposta('Resposta 1', _responder),
-            Resposta('Resposta 2', _responder),
-            Resposta('Resposta 3', _responder),*/
-
-                  /* ElevatedButton(
-              child: Text('Resposta 1'),
-              onPressed: _responder, //o void _responder fica sem (),
-              //você só chama o metodo, você não passa por parâmetro
-            ),
-            ElevatedButton(
-              child: Text('Resposta 2'),
-              onPressed: _responder,
-            ),
-            ElevatedButton(
-              child: Text('Resposta 3'),
-              onPressed: _responder,
-              /* funcaoQueRetornaOutraFuncao() nesse caso, uma função chama outra função,*/
-            ),*/
-                ],
+            ? Questionario(
+                perguntas: _perguntas,
+                perguntaSelecionada: _perguntaSelecionada,
+                quantoResponder: _responder,
               )
             : Resultado(),
       ),
@@ -99,3 +71,22 @@ onde qualquer arquivo externo não consiga acessar a classe
 
 ctrl D seleciona os caracteres iguais
 */
+
+/* Resposta('Resposta 1', _responder),
+            Resposta('Resposta 2', _responder),
+            Resposta('Resposta 3', _responder),*/
+
+/* ElevatedButton(
+              child: Text('Resposta 1'),
+              onPressed: _responder, //o void _responder fica sem (),
+              //você só chama o metodo, você não passa por parâmetro
+            ),
+            ElevatedButton(
+              child: Text('Resposta 2'),
+              onPressed: _responder,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 3'),
+              onPressed: _responder,
+              /* funcaoQueRetornaOutraFuncao() nesse caso, uma função chama outra função,*/
+            ),*/
